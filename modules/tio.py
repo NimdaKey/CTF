2b5f7659d1d42ef692b48a586053d01e8b79da55
NimdaKey <nimdakey@gmail.com>
2014-10-13 10:21:18 +0800

    Update tio.py

--- modules/tio.py
+++ modules/tio.py
@@ -1,28 +1,16 @@
 import telnetlib
 import re
 
-class tio:
+class tio(telnetlib.Telnet):
     def __init__(self,host='127.0.0.1',port=23, timeout=30):
-        self.host = host
-        self.port = port
-        self.timeout = timeout
-        self.io = telnetlib.Telnet(self.host, self.port, self.timeout)
+        self.io=telnetlib.Telnet.__init__(self,host,port,timeout)
 
     def send(self,data):
         return self.io.write(data)
 
-    def read_until(self,data):
-        return self.io.read_until(data)
-    
-    def read_all(self):
-        return self.io.read_all()
-
     def read_until_re(self,pattern):
         content = self.io.read_all()
         return re.search(pattern, content)
-        
-    def close(self):
-        self.io.close()
         
 '''
 # usage:
